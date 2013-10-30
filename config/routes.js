daeb02deb6bc340043d8db4e80d4f00b927d9793
Nathan Woodbury <nathanswoodbury@gmail.com>
2013-10-30 09:18:47 -0600

    Backend routes for agents added (unsecured).

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,9 @@
-var async = require('async');
-
 module.exports = function(app, passport, auth) {
 
     //=========================================================================
-    // Users
+    //  Users
     //=========================================================================
-    
+
     //User Routes
     var users = require('../app/controllers/users');
     app.get('/signin', users.signin);
@@ -19,7 +17,7 @@ module.exports = function(app, passport, auth) {
         failureRedirect: '/signin',
         failureFlash: 'Invalid email or password.'
     }), users.session);
-    
+
     // Public User Routes
     app.get('/users', users.all);
     app.get('/users/me', users.me);
@@ -29,9 +27,9 @@ module.exports = function(app, passport, auth) {
             auth.user.hasAuthorization); //Insecure?
 
     //=========================================================================
-    // Auth
+    //  Auth
     //=========================================================================
-    
+
     //Setting the facebook oauth routes
     app.get('/auth/facebook', passport.authenticate('facebook', {
         scope: ['email', 'user_about_me'],
@@ -77,29 +75,29 @@ module.exports = function(app, passport, auth) {
     app.param('userId', users.user);
 
     //=========================================================================
-    // Articles
+    //  Articles
     //=========================================================================
 
     var articles = require('../app/controllers/articles');
     app.get('/articles', articles.all);
     app.post('/articles', auth.requiresLogin, articles.create);
     app.get('/articles/:articleId', articles.show);
-    app.put('/articles/:articleId', auth.requiresLogin, 
+    app.put('/articles/:articleId', auth.requiresLogin,
             auth.article.hasAuthorization, articles.update);
-    app.del('/articles/:articleId', auth.requiresLogin, 
+    app.del('/articles/:articleId', auth.requiresLogin,
             auth.article.hasAuthorization, articles.destroy);
 
     //Finish with setting up the articleId param
     app.param('articleId', articles.article);
 
     //=========================================================================
-    // Singledocs
+    //  Singledocs
     //=========================================================================
 
     var singledocs = require('../app/controllers/singledocs');
     app.get('/docs', singledocs.all);
     app.post('/docs', auth.requiresLogin, singledocs.create);
-    app.get('/docs/:singledocId', auth.requiresLogin, 
+    app.get('/docs/:singledocId', auth.requiresLogin,
             auth.singledoc.hasAuthorization, singledocs.show);
     app.put('/docs/:singledocId', auth.requiresLogin,
             auth.singledoc.hasAuthorization, singledocs.update);
@@ -110,7 +108,22 @@ module.exports = function(app, passport, auth) {
     app.param('singledocId', singledocs.singledoc);
 
     //=========================================================================
-    // Home
+    //  Agents
+    //=========================================================================
+
+    var agents = require('../app/controllers/agents');
+    app.get('/agents', agents.all);
+    app.post('/agents', auth.requiresLogin, agents.create);
+    app.get('/agents/:agentId', auth.requiresLogin, agents.show);
+    app.put('/agents/:agentId', auth.requiresLogin, agents.update);
+    app.del('/agents/:agentId', auth.requiresLogin, agents.destroy);
+
+    // Finish with setting up the singledocId param
+    app.param('agentId', singledocs.singledoc);
+
+
+    //=========================================================================
+    //  Home
     //=========================================================================
 
     //Home route
